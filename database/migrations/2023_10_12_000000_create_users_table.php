b54d3b95086768da19b41c4559b38aaa1a160770
Gabriel Moreira <j.gabriel.21f@gmail.com>
2022-11-22 16:45:50 -0300

    Tabela users, empresas e categorias

--- database/migrations/2023_10_12_000000_create_users_table.php
+++ database/migrations/2023_10_12_000000_create_users_table.php
@@ -19,9 +19,10 @@ return new class extends Migration
             $table->string('matricula')->unique();
             $table->string('password');
             $table->integer('categoria_id')->unsigned();
-            $table->foreign('categoria_id')->references('id')->on('usuario_categorias');
-            $table->string('empresa');
-            $table->string('state');
+            $table->foreign('categoria_id')->references('id')->on('user_categorias');
+            $table->integer('empresa_id')->unsigned();
+            $table->foreign('empresa_id')->references('id')->on('empresas');
+            $table->string('status')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
